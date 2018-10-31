7eb9ca340e36a5d08bcba705c84a8d371ded661f
Flannon Jackson <flannon@nyu.edu>
2018-10-31 16:20:53 -0400

    Register uid from run and added debugging

--- bin/getuid.sh
+++ bin/getuid.sh
@@ -9,7 +9,10 @@ set -eux
 #  fi
 #fi
 
+grep default /etc/passwd
+
 [[ ! $(whoami 2> /dev/null) ]] && \
   [[ -w /etc/passwd ]] && \
     echo "${USER_NAME:-runner}:x:$(id -u):0:Container Application User:${HOME}:/sbin/nologin" >> /etc/passwd
    
+grep default /etc/passwd
